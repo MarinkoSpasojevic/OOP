2c1725bc9ffb4dabc43f1095c28fc9de35153e78
Marinko <marinkospasojevic@gmail.com>
2018-07-12 10:35:37 +0200

    Classes and constructors

---
+++ Classes and constructors/Example1/Example1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student(); //default constructor
+
+            Student student1 = new Student("John", "Doe");//overloaded constructor
+            Console.WriteLine(student1.GetFullName());
+        }
+    }
+}
