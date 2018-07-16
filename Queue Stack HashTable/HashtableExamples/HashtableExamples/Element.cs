7521ecdf016d286529009566c06ebe3181f08b07
Marinko <marinkospasojevic@gmail.com>
2018-07-16 09:36:04 +0200

    HashTable finished

---
+++ Queue Stack HashTable/HashtableExamples/HashtableExamples/Element.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashtableExamples
+{
+    public enum Element
+    {
+        First,
+        Second,
+        Third
+    }
+}
